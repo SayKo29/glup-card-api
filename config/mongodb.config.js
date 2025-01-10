ce427680f50eb60d5312fdf5c7ce87614cb3f322
SayKo29 <aaronsar29@gmail.com>
2025-01-10 21:32:36 +0100

    first version

--- config/mongodb.config.js
+++ config/mongodb.config.js
@@ -2,15 +2,9 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", true);
 
-mongoose.connect(
-    process.env.DATABASE_URL,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    (err) => {
-        if (err) {
-            console.log("Database connection failed.");
-        }
-        console.log("Successfully connected to MongoDB.");
-    }
-);
+mongoose
+    .connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Successfully connected to MongoDB."))
+    .catch((err) => console.log("Database connection failed.", err));
 
-export default mongoose;
+module.exports = mongoose;
